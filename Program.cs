42e4e561b8db61b5fdd61ecf7cc21c57ee02a606
Shatpoz1288 <gami95@outlook.com>
2020-09-23 21:09:11 -0400

    mensaje de paola

--- Program.cs
+++ Program.cs
@@ -7,9 +7,9 @@ namespace IRCalculator
         static void Main(string[] args)
         {
             Console.Write("Enter loan amount: ");
-            var loan = Int32.Parse(Console.ReadLine());
+            int loan = Int32.Parse(Console.ReadLine());
             Console.Write("\nEnter time in years: ");
-            var years = Int32.Parse(Console.ReadLine());
+            int years = Int32.Parse(Console.ReadLine());
             Console.Write("\nEnter interest rate: ");
             float interestRate = float.Parse(Console.ReadLine());
 
